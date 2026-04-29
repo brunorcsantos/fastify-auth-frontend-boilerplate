5c088ef91df77b491c11c0843d236651edec9a00
unknown <brunorobertocezarsantos@gmail.com>
2026-04-29 18:43:29 -0300

    feat: add dashboard page with user info cards

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,17 +1,133 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
-import { useEffect } from "react";
-import DashboardLayout from "../layouts/DashboardLayout";
+import { FiUser, FiCalendar, FiShield, FiSettings } from "react-icons/fi";
 
 const Dashboard = () => {
   const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const dateConverter = (date: string) => {
+    const isoString = date;
+    const data = new Date(isoString!);
+    const formatada = data.toLocaleString("pt-BR");
+    return formatada;
+  };
+
+  const handleLogout = async () => {
+    await logout({ id: user!.id });
+    navigate("/login");
+  };
 
   return (
-    <>
-      <span>{user!.id}</span>
-      <span>{user!.email}</span>
-      <span>{user!.name}</span>
-    </>
+    <div>
+      <div className="flex flex-col gap-1 bg-card border border-border rounded-xl p-6 shadow-sm mb-6 ">
+        <span
+          style={{ fontFamily: "Space Grotesk" }}
+          className="text-2xl font-bold text-foreground"
+        >
+          Olá, {user!.name}!
+        </span>
+        <span className=" text-muted-foreground">Bem-vindo ao seu painel</span>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {/*Perfil*/}
+        <div className="bg-card border border-border rounded-xl p-6 shadow-sm">
+          {/* cabeçalho do card */}
+          <div className="flex items-center gap-2 mb-4">
+            <FiUser />
+            <p className="font-semibold">Perfil</p>
+          </div>
+          {/* conteúdo do card */}
+          <div className="flex flex-col gap-2">
+            {/* pares label + valor virão aqui */}
+            <div className="flex flex-col">
+              <span className="text-xs text-muted-foreground">Nome</span>
+              <span className="text-sm text-foreground">{user!.name}</span>
+            </div>
+            <div className="flex flex-col">
+              <span className="text-xs text-muted-foreground">Email</span>
+              <span className="text-sm text-foreground">{user!.email}</span>
+            </div>
+            <div className="flex flex-col">
+              <span className="text-xs text-muted-foreground">Provedor</span>
+              <span className="text-sm text-foreground">{user!.provider}</span>
+            </div>
+          </div>
+        </div>
+        {/*Conta */}
+        <div className="bg-card border border-border rounded-xl p-6 shadow-sm">
+          {/* cabeçalho do card */}
+          <div className="flex items-center gap-2 mb-4">
+            <FiCalendar />
+            <p className="font-semibold">Conta </p>
+          </div>
+          {/* conteúdo do card */}
+          <div className="flex flex-col gap-2">
+            {/* pares label + valor virão aqui */}
+            <div className="flex flex-col">
+              <span className="text-xs text-muted-foreground">Criado em</span>
+              <span className="text-sm text-foreground">
+                {dateConverter(user!.createdAt)}
+              </span>
+            </div>
+            <div className="flex flex-col">
+              <span className="text-xs text-muted-foreground">
+                Última Atualização
+              </span>
+              <span className="text-sm text-foreground">
+                {dateConverter(user!.updatedAt)}
+              </span>
+            </div>
+          </div>
+        </div>
+        {/*Sessão */}
+        <div className="bg-card border border-border rounded-xl p-6 shadow-sm">
+          {/* cabeçalho do card */}
+          <div className="flex items-center gap-2 mb-4">
+            <FiShield />
+            <p className="font-semibold">Sessão</p>
+          </div>
+          {/* conteúdo do card */}
+          <div className="flex flex-col gap-2">
+            {/* pares label + valor virão aqui */}
+            <div className="flex flex-col">
+              <span className="text-xs text-muted-foreground">ID</span>
+              <span className="text-sm text-foreground">{user!.id}</span>
+            </div>
+            <div className="flex flex-col">
+              <span className="text-xs text-muted-foreground">Tipo</span>
+              <span className="text-sm text-foreground">
+                {user!.provider === "local" ? "Email/Senha" : "OAuth"}
+              </span>
+            </div>
+          </div>
+        </div>
+        {/*Ações */}
+        <div className="bg-card border border-border rounded-xl p-6 shadow-sm">
+          {/* cabeçalho do card */}
+          <div className="flex items-center gap-2 mb-4">
+            <FiSettings />
+            <p className="font-semibold">Ações </p>
+          </div>
+          {/* conteúdo do card */}
+          <div className="flex flex-col gap-2">
+            {/* pares label + valor virão aqui */}
+            <Link
+              to="/profile"
+              className="flex items-center justify-center cursor-pointer py-2 bg-primary text-primary-foreground font-semibold border border-border rounded-xl"
+            >
+              Editar Perfil
+            </Link>
+            <button
+              className="cursor-pointer py-2 bg-destructive text-primary-foreground font-semibold border border-border rounded-xl"
+              onClick={handleLogout}
+            >
+              Sair
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
